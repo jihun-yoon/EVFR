4b5792e87ddcce33c395daf043e3c0507ebfa683
Jihun Yoon <yjh4374@gmail.com>
2025-03-03 09:47:47 +0000

    refactor: modify setup and requirements with more packages

--- setup.py
+++ setup.py
@@ -3,12 +3,27 @@ import setuptools
 setuptools.setup(
     name="evfr",
     version="0.1.0",
-    author="Your Name",
-    description="Exploring Video Frame Redundancy library.",
+    author="Jihun Yoon",
+    author_email="yjh4374@gmail.com",
+    description="Exploring Video Frame Redundancy library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jihun-yoon/EVFR",
     packages=setuptools.find_packages(),
     python_requires=">=3.10",
     install_requires=[
         "numpy==2.2.3",
-        # add other dependencies here
+        "transformers==4.49.0",
+        "Pillow==11.1.0",
+        "tqdm==4.67.1",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Multimedia :: Video",
     ],
 )
