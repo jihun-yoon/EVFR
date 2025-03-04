77a8149efb248171f99b86a693c14c39b104bb4b
Jihun Yoon <yjh4374@gmail.com>
2025-03-04 08:46:16 +0000

    feat: new fps algorithm with FAISS

--- evfr/sampling/__init__.py
+++ evfr/sampling/__init__.py
@@ -1,3 +1,12 @@
 """
+# evfr/sampling/__init__.py
+--------------------
 Sampling algorithms for EVFR.
 """
+
+from .fps_faiss import farthest_point_sampling_faiss
+
+__all__ = [
+    "farthest_point_sampling_faiss",
+    # ...
+]
